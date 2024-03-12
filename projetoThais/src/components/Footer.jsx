94256437aaed0bc18930e515fb8841cfe9ebb565
Luiz Filipe <99333746+Filipee01@users.noreply.github.com>
2024-03-11 22:08:07 -0300

    update

--- projetoThais/src/components/Footer.jsx
+++ projetoThais/src/components/Footer.jsx
@@ -2,15 +2,7 @@ export default function Footer() {
 
     return (
       <footer className="contacte">
-        <h3 className="footer-finish">Entre emmmmm contatoooo:</h3>
-        <div className="contact">
-        <a href="https://github.com/Filipee01" target="_blank"><img src="Images/icons8-github (2).svg" alt="github"/></a>
-        <a href="https://wa.me/5581989774161" target="_blank"><img src="Images/icons8-whatsapp (1).svg" alt="wpp" /></a>
-        <a href="https://www.linkedin.com/in/filipee01/" target="_blank"><img src="Images/icons8-linkedin-circundado.svg" alt="linkedin" /></a>
-        </div>
-        <div className="finish">
-        <p>Site criado por &copy; Luiz Filipe</p>
-        </div>
+        <h3 className="footer-finish">Entre em Contato:</h3>
         
       </footer>
     )
--- projetoThais/src/components/Header.jsx
+++ projetoThais/src/components/Header.jsx
@@ -1,15 +1,17 @@
 import {Link} from "react-router-dom"
+import Logo from "../assets/logo/logo.svg"
 
 export default function Header(){
   return (
 
       <header className="navegation">
     <div className="logo">
-      <img src="#" alt="fsafsafsafafsa" />
+      <img src={Logo} alt="Logo" />
     </div>
       <nav>
-        <ul>
+        <ul className="container-nav">
           <Link to = {'/'} className="nav-link"> Inicio </Link> 
+          <Link to = {'/timeline'} className="nav-link"> Linha do Tempo </Link> 
           <Link to = {'/career'} className="nav-link"> Carreiras </Link> 
           <Link to = {'/game'} className="nav-link"> Game</Link> 
         </ul>
